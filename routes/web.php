06d5237e70001925025d5a5ee0977c6191afec9d
Adrian Reyhan <adrianreyhan03.ar@gmail.com>
2023-11-05 08:02:21 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+    
 });
+
+Route::get('login', 'App\Http\Controllers\AuthController@login')->name('login');
