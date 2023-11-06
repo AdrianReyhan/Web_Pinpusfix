94e46607bd625455b7c2e9988aa131378018b4df
sufyanhanif <hanifsufyan48@gmail.com>
2023-11-06 11:28:39 +0700

    login awal

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
+
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-    
-});
+Route::view('/', 'auth.login');
+
+
 
 Route::get('login', 'App\Http\Controllers\AuthController@login')->name('login');
 
