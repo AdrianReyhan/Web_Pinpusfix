226ecf2a74d3b46ea5236e88ee2d4e50ed99b093
Adrian Reyhan <adrianreyhan03.ar@gmail.com>
2023-11-10 16:47:50 +0700

    konek

--- resources/views/layouts/mahasiswa/sidebar.blade.php
+++ resources/views/layouts/mahasiswa/sidebar.blade.php
@@ -3,7 +3,7 @@
         <div class="sidebar-brand-icon">
             <img src="{{ asset('/img/logo.png') }}">
         </div>
-        <div class="sidebar-brand-text mx-3 my-0"style="font-size:10pt">Politeknik Negeri Semarang</div>
+        <div class="sidebar-brand-text mx-3 my-0"style="font-size:9pt">Politeknik Negeri Semarang</div>
     </a>
     <hr class="sidebar-divider my-0">
     <li class="nav-item">
@@ -16,24 +16,6 @@
         Features
 
     </div>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrap"
-            aria-expanded="true" aria-controls="collapseBootstrap">
-            <i class="fa-solid fa-users"></i>
-            <span>Mahasiswa</span>
-        </a>
-        <div id="collapseBootstrap" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Barang</h6>
-                <a class="collapse-item" href="/buku">Lihat Mahasiswa</a>
-                <a class="collapse-item" href="/buku">Tambah Mahasiswa</a>
-                @if (Auth::user()->isAdmin == 1)
-                    <a class="collapse-item" href="/buku/create">Tambah Barang</a>
-                @endif
-
-            </div>
-        </div>
-    </li>
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseForm"
             aria-expanded="true" aria-controls="collapseForm">
@@ -52,43 +34,6 @@
             </div>
         </div>
     </li>
-
-    @if (Auth::user()->isAdmin == 1)
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTable"
-                aria-expanded="true" aria-controls="collapseTable">
-                <i class="fa-solid fa-users"></i>
-                <span>Anggota</span>
-            </a>
-            <div id="collapseTable" class="collapse" aria-labelledby="headingTable" data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Anggota</h6>
-                    <a class="collapse-item" href="/anggota">Lihat Anggota</a>
-                    <a class="collapse-item" href="/anggota/create">Tambah Anggota</a>
-                </div>
-            </div>
-        </li>
-    @endif
-
-    @if (Auth::user()->isAdmin == 1)
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePeminjam"
-                aria-expanded="true" aria-controls="collapsePeminjam">
-                <i class="fa-solid fa-user-pen"></i>
-                <span>Peminjaman</span>
-            </a>
-            <div id="collapsePeminjam" class="collapse" aria-labelledby="headingPeminjam"
-                data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Peminjaman</h6>
-                    <a class="collapse-item" href="/peminjaman">Riwayat Peminjaman</a>
-                    <a class="collapse-item" href="/peminjaman/create">Tambahkan Peminjaman</a>
-                    <a class="collapse-item" href="/pengembalian">Kembalikan Barang</a>
-                </div>
-            </div>
-        </li>
-    @endif
-
     @if (Auth::user()->isAdmin == 0)
         <li class="nav-item">
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePeminjam"
